904a98d82abb0ba3e5e54c557671b846eb035c60
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-04-11 16:31:55 +0200

    Adding eventlisteners on booking buttons

--- script.js
+++ script.js
@@ -3,6 +3,12 @@
 const form = document.getElementById('bookingForm');
 form.addEventListener('submit', (e) => loadResults(e));
 
+const cancelBookingBtn = document.getElementById('cancel');
+const confirmBookingBtn = document.getElementById('confirm');
+
+cancelBookingBtn.addEventListener('click', (e) => cancelBookingBtn(e));
+confirmBookingBtn.addEventListener('click', (e) => confirmBooking(e));
+
 function loadResults(e) {
     e.preventDefault();
 
@@ -16,4 +22,14 @@ function loadResults(e) {
     plusBtn.style.top = '33%';
     minusBtn.style.top = '42%';
     filterBtn.style.display = 'block';
+}
+
+function cancelBooking(e) {
+
+}
+
+function confirmBooking(e) {
+    const dialog = document.createElement('dialog');
+
+    //Todo fill out dialog
 }
