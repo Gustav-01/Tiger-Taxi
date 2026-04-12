86bde735f0c152d7c632f03cb0700a2abdc76f1e
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-04-12 21:23:29 +0200

    Cancel booking event function styling done.

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ form.addEventListener('submit', (e) => loadResults(e));
 const cancelBookingBtn = document.getElementById('cancel');
 const confirmBookingBtn = document.getElementById('confirm');
 
-cancelBookingBtn.addEventListener('click', (e) => cancelBookingBtn(e));
+cancelBookingBtn.addEventListener('click', (e) => cancelBooking(e));
 confirmBookingBtn.addEventListener('click', (e) => confirmBooking(e));
 
 function loadResults(e) {
     e.preventDefault();
+
+    const submitBtn = document.getElementById('submit');
+    submitBtn.disabled = true;
 
     const resultBox = document.getElementsByClassName('drivers')[0];
     resultBox.style.display = 'block';
@@ -25,13 +28,22 @@ function loadResults(e) {
 }
 
 function cancelBooking(e) {
+    const submitBtn = document.getElementById('submit');
+    submitBtn.disabled = false;
 
+    const resultBox = document.getElementsByClassName('drivers')[0];
+    resultBox.style.display = 'none';
+    const plusBtn = document.getElementById('plus');
+    const minusBtn = document.getElementById('minus');
+    const filterBtn = document.getElementById('filter');
+
+    plusBtn.style.top = '83%';
+    minusBtn.style.top = '92%';
+    filterBtn.style.display = 'none';
 }
 
 function confirmBooking(e) {
 
-    console.log('Popup confirmation');
-    
     const dialog = document.createElement('dialog');
     dialog.closedBy = 'any';
     dialog.className = 'bookingConfirmation';
@@ -40,19 +52,19 @@ function confirmBooking(e) {
     img.src = 'assets/checkmark.png';
     img.alt = 'Grön checkbock';
     dialog.appendChild(img);
-    
+
     const h2 = document.createElement('h2');
     h2.textContent = 'Bokning bekräftad'
     dialog.appendChild(h2);
-    
+
     const p = document.createElement('p');
     p.textContent = 'Information om bokningen';
     dialog.appendChild(p);
-    
+
     const doneBtn = document.createElement('button');
     doneBtn.textContent = 'Klar';
     dialog.appendChild(doneBtn);
-    
+
     document.getElementsByTagName('main')[0].append(dialog);
 
     dialog.showModal();
