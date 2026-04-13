c409198c32fb075d0e28d3a23e91b8f296828204
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2026-04-13 14:07:12 +0200

    fixing cancel function

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 
-
 const form = document.getElementById('bookingForm');
 form.addEventListener('submit', (e) => loadResults(e));
 
 const cancelBookingBtn = document.getElementById('cancel');
 const confirmBookingBtn = document.getElementById('confirm');
 
-cancelBookingBtn.addEventListener('click', (e) => cancelBookingBtn(e));
+cancelBookingBtn.addEventListener('click', (e) => cancelBooking(e));
 confirmBookingBtn.addEventListener('click', (e) => confirmBooking(e));
 
 function loadResults(e) {
     e.preventDefault();
 
+    const submitBtn = document.getElementById('submit');
+    submitBtn.disabled = true;
+
     const resultBox = document.getElementsByClassName('drivers')[0];
     resultBox.style.display = 'block';
 
-    document.querySelectorAll('.taxi').forEach( taxi => {taxi.style.display = 'block'});
+    document.querySelectorAll('.taxi').forEach(taxi => { taxi.style.display = 'block' });
 
     const plusBtn = document.getElementById('plus');
     const minusBtn = document.getElementById('minus');
@@ -27,13 +29,24 @@ function loadResults(e) {
 }
 
 function cancelBooking(e) {
+    const submitBtn = document.getElementById('submit');
+    submitBtn.disabled = false;
 
+    document.querySelectorAll('.taxi').forEach(taxi => { taxi.style.display = 'none' });
+
+    const resultBox = document.getElementsByClassName('drivers')[0];
+    resultBox.style.display = 'none';
+    const plusBtn = document.getElementById('plus');
+    const minusBtn = document.getElementById('minus');
+    const filterBtn = document.getElementById('filter');
+
+    plusBtn.style.top = '83%';
+    minusBtn.style.top = '92%';
+    filterBtn.style.display = 'none';
 }
 
 function confirmBooking(e) {
 
-    console.log('Popup confirmation');
-    
     const dialog = document.createElement('dialog');
     dialog.closedBy = 'any';
     dialog.className = 'bookingConfirmation';
@@ -42,19 +55,19 @@ function confirmBooking(e) {
     img.src = 'assets/checkmark.png';
     img.alt = 'Grön checkbock';
     dialog.appendChild(img);
-    
+
     const h2 = document.createElement('h2');
     h2.textContent = 'Bokning bekräftad'
     dialog.appendChild(h2);
-    
+
     const p = document.createElement('p');
     p.textContent = 'Information om bokningen';
     dialog.appendChild(p);
-    
+
     const doneBtn = document.createElement('button');
     doneBtn.textContent = 'Klar';
     dialog.appendChild(doneBtn);
-    
+
     document.getElementsByTagName('main')[0].append(dialog);
 
     dialog.showModal();
